0fbc38361bb673809ed49fc187a67068cb81f787
Sunday Mgbogu <digitalsimboja@gmail.com>
2024-01-10 11:31:52 +0100

    Fix typo

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,7 +4,7 @@ const Hero = () => {
     <div className="mt-16 py-2 px-4 flex justify-around items-center">
       <div className="flex flex-col justify-center md:max-w-xl">
         <h1 className="text-4xl md:text-7xl font-bold mb-4 whitespace-pre-line leading-tight">
-          Evveland AI, Generative AI Platform on the Metaverse
+          Evveland AI, Generative AI Platform for the Metaverse
         </h1>
         <p className="text-sm text-gray-500">
           Create and Deploy custom AI Agents, Chatbots and Virtual Assistants
