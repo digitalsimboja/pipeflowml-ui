e4d99719c3dc71bf52290d2f68d75dd1c89cc548
Sunday Mgbogu <digitalsimboja@gmail.com>
2024-01-12 18:07:05 +0100

    Make the hero page responsive

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,30 +4,28 @@ import LiveDemoButton from "./common/liveDemoButton";
 
 const Hero = () => {
   return (
-    <div className="mt-8 px-4 flex flex-col items-center">
-      <div className="flex flex-col items-center mb-4">
-        <h1 className="text-2xl md:text-4xl lg:text-7xl font-bold mb-2 text-center">
+    <div className="mt-16 px-4 py-8 md:flex md:justify-around items-center relative z-0">
+      <div className="max-w-xl text-left mb-8 md:mb-0">
+        <h1 className="text-4xl md:text-7xl font-bold mb-4 whitespace-pre-line leading-tight">
           Evveland AI, Generative AI Platform for the Metaverse
         </h1>
-        <p className="text-sm text-gray-500 text-center">
+        <p className="text-sm text-gray-500 mb-4">
           Create and Deploy custom AI Agents, Chatbots and Virtual Assistants
           using your business content.
         </p>
+        <div className="flex flex-col items-center md:flex-row md:justify-center gap-4">
+          <GetStartedButton text="Get Started" />
+          <LiveDemoButton text="Live Demo" />
+        </div>
       </div>
-      <div className="flex flex-col items-center mb-4">
+      <div className="flex-shrink-0">
         <Image
-          width={300}
-          height={300}
+          width={600}
+          height={600}
           src="/images/hero_image.png"
           alt="Evveland AI platform Landing page image"
           className="w-full border-none rounded"
         />
-      </div>
-      <div className="flex flex-col items-center mb-4">
-        <div className="flex flex-col items-center gap-4">
-          <GetStartedButton text="Get Started" />
-          <LiveDemoButton text="Live Demo" />
-        </div>
       </div>
     </div>
   );
