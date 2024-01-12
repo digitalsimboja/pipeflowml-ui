1ad2a21ade5dd64a3392a74a09e3d03f7c846215
Sunday Mgbogu <digitalsimboja@gmail.com>
2024-01-12 17:42:27 +0100

    Make the Login form mobile responsive

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -9,10 +10,11 @@ const Login = () => {
   const handleLogin = () => {
     console.log(email + password);
   };
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen ">
-      <div className="w-[500px]  flex flex-col p-4">
-        <div className="flex items-center justify-center mb-2 ">
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="w-full md:w-[500px] flex flex-col p-4">
+        <div className="flex items-center justify-center mb-4">
           <Link href="/">
             <Image
               width={200}
@@ -22,12 +24,24 @@ const Login = () => {
             />
           </Link>
         </div>
-        <div className=" gp-4 mb-4">
-          <h1 className="font-bold text-2xl mb-2">Login</h1>
-          <div className="flex gap-2"><p>Don't have an account yet? </p> <Link href="/signup" className="text-blue-900 font-bold ">Sign up</Link></div>
+        <div className="mb-4">
+          <h1 className="font-bold text-2xl mb-2 text-center">Login</h1>
+          <div className="flex flex-col gap-2 text-center">
+            <p>Don't have an account yet? </p>{" "}
+            <Link href="/signup" className="text-blue-900 font-bold">
+              Sign up
+            </Link>
+          </div>
         </div>
-        <div className=" mb-4 p-8 bg-white rounded-md shadow-md w-[500px]">
-          <form className="flex flex-col items-center ">
+        <div className="mb-4 p-4 bg-white rounded-md shadow-md">
+          <form className="flex flex-col">
+            <label
+              htmlFor="email"
+              className="text-sm font-medium text-gray-600 block mb-2"
+            >
+              Email address
+            </label>
+
             <input
               type="email"
               placeholder="Email"
@@ -35,15 +49,21 @@ const Login = () => {
               onChange={(e) => setEmail(e.target.value)}
               className="border rounded-md p-2 mb-2 w-full"
             />
+            <label
+              htmlFor="password"
+              className="text-sm font-medium text-gray-600 block mb-2"
+            >
+              Password
+            </label>
             <input
               type="password"
               placeholder="Password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="border p-2 mb-4 rounded-md w-full"
+              className="border p-2 mb-2 rounded-md w-full"
             />
             <button
-              className="border p-2 rounded-md w-[80%] bg-blue-100 text-black tracking-wide text-xl font-bold"
+              className="border p-2 rounded-md w-full bg-blue-100 text-black tracking-wide text-xl font-bold"
               onClick={handleLogin}
             >
               Login
